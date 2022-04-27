e229452d13f128e343a1cac8cee5a88a099667f2
bigfile57 <bigfile57@naver.com>
2022-04-27 23:57:42 +0900

    tailwind, js

---
+++ 0427/002.js
@@ -0,0 +1,6 @@
+
+const one = document.getElementById('one');
+one.innerHTML = "hello <strong>world document</strong>";
+console.log("hellow world console: "+ one);
+console.log(one);
+window.alert("hello world alert");
